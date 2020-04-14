f948311e79e7f91cbc6d2b9faf4521bcd05c444c
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2020-04-14 20:06:12 +0430

    CORE: Add core functions

---
+++ src/_core/getFullYear/index.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * @param cleanDate {Date}
+ * @returns {number}
+ */
+export default function getFullYear(cleanDate) {
+  return cleanDate.getFullYear()
+}
---
+++ src/_core/newDate/index.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * @param args
+ * @returns {Date}
+ */
+export default function newDate(...args) {
+  return new Date(...args)
+}
