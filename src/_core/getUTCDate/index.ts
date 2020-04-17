d0f52d14d72ebd85e984891b8129752e8c6415ae
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2020-04-17 20:12:01 +0430

    JALALI_CORE: use jalali calendar in core functions

--- src/_core/getUTCDate/index.ts
+++ src/_core/getUTCDate/index.ts
@@ -1,8 +1,13 @@
+import { toJalali } from '../../_jalali/index'
+
 /**
  *
  * @param cleanDate {Date}
  * @returns {number}
  */
 export default function getUTCDate(cleanDate: Date) {
-  return cleanDate.getUTCDate()
+  const gd = cleanDate.getUTCDate()
+  const gm = cleanDate.getUTCMonth() + 1
+  const gy = cleanDate.getUTCFullYear()
+  return toJalali(gy, gm, gd).jd
 }
