834f45b8ae3fed00de07ff9d44c8206f2961d2f7
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2020-04-17 20:12:01 +0430

    JALALI_CORE: use jalali calendar in core functions

--- src/_core/setUTCDate/index.ts
+++ src/_core/setUTCDate/index.ts
@@ -1,3 +1,5 @@
+import {toGregorian, toJalali} from "../../_jalali/index";
+
 /**
  *
  * @param cleanDate {Date}
@@ -8,5 +10,10 @@ export default function setUTCDate(
   cleanDate: Date,
   ...args: Parameters<Date['setUTCDate']>
 ) {
-  return cleanDate.setUTCDate(...args)
-}
+  const gd = cleanDate.getUTCDate()
+  const gm = cleanDate.getUTCMonth() + 1
+  const gy = cleanDate.getUTCFullYear()
+  const j = toJalali(gy, gm, gd)
+  const [date] = args
+  const g = toGregorian(j.jy, j.jm, date)
+  return cleanDate.setUTCFullYear(g.gy, g.gm - 1, g.gd)}
--- src/_core/setUTCFullYear/index.ts
+++ src/_core/setUTCFullYear/index.ts
@@ -1,3 +1,5 @@
+import { toGregorian, toJalali } from '../../_jalali/index'
+
 /**
  *
  * @param cleanDate {Date}
@@ -8,5 +10,11 @@ export default function setUTCFullYear(
   cleanDate: Date,
   ...args: Parameters<Date['setUTCFullYear']>
 ) {
-  return cleanDate.setUTCFullYear(...args)
+  const gd = cleanDate.getUTCDate()
+  const gm = cleanDate.getUTCMonth() + 1
+  const gy = cleanDate.getUTCFullYear()
+  const j = toJalali(gy, gm, gd)
+  const [year, month = j.jm, date = j.jd] = args
+  const g = toGregorian(year, month + 1, date)
+  return cleanDate.setUTCFullYear(g.gy, g.gm - 1, g.gd)
 }
