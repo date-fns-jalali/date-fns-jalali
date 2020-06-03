f186c2934fe90dc5aba9d1f46d6d080500e75983
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2020-06-04 01:40:09 +0430

    update types

--- src/isFuture/index.d.ts
+++ src/isFuture/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { isFuture } from 'date-fns'
+import { isFuture } from 'date-fns-jalali'
 export default isFuture
