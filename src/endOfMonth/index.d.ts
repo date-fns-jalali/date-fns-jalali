d2c29c8afb0d304eaca77d96875a76cb75d1e267
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-02-09 12:20:51 +0330

    TYPES: update types

--- src/endOfMonth/index.d.ts
+++ src/endOfMonth/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { endOfMonth } from 'date-fns'
+import { endOfMonth } from 'date-fns-jalali'
 export default endOfMonth
