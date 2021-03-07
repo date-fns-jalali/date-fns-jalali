cd119dad3d43ada46a9bff071b0ec43c58d947e1
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-03-07 13:39:17 +0330

    TYPES: update types

--- src/locale/ug/index.d.ts
+++ src/locale/ug/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { ug } from 'date-fns/locale'
+import { ug } from 'date-fns-jalali/locale'
 export default ug
