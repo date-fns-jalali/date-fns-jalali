656fd25b39a75480c9f72a1bfc40ae8e664a71ff
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-03-17 13:30:10 +0330

    DEFAULT_LOCALE: add default locale

--- src/formatDistanceStrict/index.js
+++ src/formatDistanceStrict/index.js
@@ -2,7 +2,7 @@ import getTimezoneOffsetInMilliseconds from '../_lib/getTimezoneOffsetInMillisec
 import compareAsc from '../compareAsc/index'
 import toDate from '../toDate/index'
 import cloneObject from '../_lib/cloneObject/index'
-import defaultLocale from '../locale/en-US/index'
+import defaultLocale from '../locale/_default/index'
 import requiredArgs from '../_lib/requiredArgs/index'
 
 var MILLISECONDS_IN_MINUTE = 1000 * 60
