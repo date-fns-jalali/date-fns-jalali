12901fcfe67fdffc6f119b580353864af77ca87e
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-03-17 13:38:59 +0330

    REMOVE_LOCALES: remove other locales

--- src/locale/fa-IR/_lib/formatRelative/index.js
+++ src/locale/fa-IR/_lib/formatRelative/index.js
@@ -1,10 +1,10 @@
 var formatRelativeLocale = {
-  lastWeek: "eeee 'گذشته در' p",
-  yesterday: "'دیروز در' p",
-  today: "'امروز در' p",
-  tomorrow: "'فردا در' p",
-  nextWeek: "eeee 'در' p",
-  other: 'P'
+  lastWeek: "'last' eeee 'at' p",
+  yesterday: "'yesterday at' p",
+  today: "'today at' p",
+  tomorrow: "'tomorrow at' p",
+  nextWeek: "eeee 'at' p",
+  other: 'P',
 }
 
 export default function formatRelative(token, _date, _baseDate, _options) {
