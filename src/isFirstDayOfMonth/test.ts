f15d5aa5d9558a050f45293b0efef9e5c69ab23f
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-04-11 14:18:29 +0430

    TEST_COMMENTS: add comments to tests

--- src/isFirstDayOfMonth/test.ts
+++ src/isFirstDayOfMonth/test.ts
@@ -6,17 +6,21 @@ import isFirstDayOfMonth from '.'
 
 describe('isFirstDayOfMonth', function () {
   it('returns true if the given date is in the last day of month', function () {
-    const result = isFirstDayOfMonth(new Date(2014, 9 /* Oct */, 1))
+    const result = isFirstDayOfMonth(
+      /* 1393/7/9 */ new Date(2014, 9 /* Oct */, 1)
+    )
     assert(result === true)
   })
 
   it('returns false if the given date is not in the last day of month', function () {
-    const result = isFirstDayOfMonth(new Date(2014, 9 /* Oct */, 2))
+    const result = isFirstDayOfMonth(
+      /* 1393/7/10 */ new Date(2014, 9 /* Oct */, 2)
+    )
     assert(result === false)
   })
 
   it('accepts a timestamp', function () {
-    const date = new Date(2014, 9 /* Oct */, 1).getTime()
+    const date = /* 1393/7/9 */ new Date(2014, 9 /* Oct */, 1).getTime()
     const result = isFirstDayOfMonth(date)
     assert(result === true)
   })
--- src/isThursday/test.ts
+++ src/isThursday/test.ts
@@ -6,17 +6,19 @@ import isThursday from '.'
 
 describe('isThursday', function () {
   it('returns true if the given date is Thursday', function () {
-    const result = isThursday(new Date(2014, 8 /* Sep */, 25))
+    const result = isThursday(/* 1393/7/3 */ new Date(2014, 8 /* Sep */, 25))
     assert(result === true)
   })
 
   it('returns false if the given date is not Thursday', function () {
-    const result = isThursday(new Date(2014, 8 /* Sep */, 24))
+    const result = isThursday(/* 1393/7/2 */ new Date(2014, 8 /* Sep */, 24))
     assert(result === false)
   })
 
   it('accepts a timestamp', function () {
-    const result = isThursday(new Date(2014, 1 /* Feb */, 13).getTime())
+    const result = isThursday(
+      /* 1392/11/24 */ new Date(2014, 1 /* Feb */, 13).getTime()
+    )
     assert(result === true)
   })
 
