2c76970559a73c45bee166fac3669b80b2d4c3df
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-04-11 12:21:48 +0430

    TEST_COMMENTS: add comments to tests

--- src/differenceInMonths/test.js
+++ src/differenceInMonths/test.js
@@ -7,24 +7,24 @@ import differenceInMonths from '.'
 describe('differenceInMonths', function () {
   it('returns the number of full months between the given dates', function () {
     var result = differenceInMonths(
-      new Date(2012, 6 /* Jul */, 2, 18, 0),
-      new Date(2011, 6 /* Jul */, 2, 6, 0)
+      /* 1391/4/12 */ new Date(2012, 6 /* Jul */, 2, 18, 0),
+      /* 1390/4/11 */ new Date(2011, 6 /* Jul */, 2, 6, 0)
     )
     assert(result === 12)
   })
 
   it('returns a negative number if the time value of the first date is smaller', function () {
     var result = differenceInMonths(
-      new Date(2011, 6 /* Jul */, 2, 6, 0),
-      new Date(2012, 6 /* Jul */, 2, 18, 0)
+      /* 1390/4/11 */ new Date(2011, 6 /* Jul */, 2, 6, 0),
+      /* 1391/4/12 */ new Date(2012, 6 /* Jul */, 2, 18, 0)
     )
     assert(result === -12)
   })
 
   it('accepts timestamps', function () {
     var result = differenceInMonths(
-      new Date(2014, 7 /* Aug */, 2).getTime(),
-      new Date(2010, 6 /* Jul */, 2).getTime()
+      /* 1393/5/11 */ new Date(2014, 7 /* Aug */, 2).getTime(),
+      /* 1389/4/11 */ new Date(2010, 6 /* Jul */, 2).getTime()
     )
     assert(result === 49)
   })
@@ -32,80 +32,80 @@ describe('differenceInMonths', function () {
   describe('edge cases', function () {
     it('it returns diff of 1 month between Feb 28 2021 and Jan 30 2021', function () {
       var result = differenceInMonths(
-        new Date(2021, 1 /* Feb */, 28),
-        new Date(2021, 0 /* Jan */, 30)
+        /* 1399/12/10 */ new Date(2021, 1 /* Feb */, 28),
+        /* 1399/11/11 */ new Date(2021, 0 /* Jan */, 30)
       )
       assert(result === 1)
     })
 
     it('it returns diff of 1 month between Feb 28 2021 and Jan 31 2021', function () {
       var result = differenceInMonths(
-        new Date(2021, 1 /* Feb */, 28),
-        new Date(2021, 0 /* Jan */, 31)
+        /* 1399/12/10 */ new Date(2021, 1 /* Feb */, 28),
+        /* 1399/11/12 */ new Date(2021, 0 /* Jan */, 31)
       )
       assert(result === 1)
     })
 
     it('it returns diff of 1 month between Nov, 30 2021 and Oct, 31 2021', function () {
       var result = differenceInMonths(
-        new Date(2021, 10 /* Nov */, 30),
-        new Date(2021, 9 /* Oct */, 31)
+        /* 1400/9/9 */ new Date(2021, 10 /* Nov */, 30),
+        /* 1400/8/9 */ new Date(2021, 9 /* Oct */, 31)
       )
       assert(result === 1)
     })
 
     it('it returns diff of 1 month between Oct, 31 2021 and Sep, 30 2021', function () {
       var result = differenceInMonths(
-        new Date(2021, 9 /* Oct */, 31),
-        new Date(2021, 8 /* Sep */, 30)
+        /* 1400/8/9 */ new Date(2021, 9 /* Oct */, 31),
+        /* 1400/7/8 */ new Date(2021, 8 /* Sep */, 30)
       )
       assert(result === 1)
     })
 
     it('it returns diff of 6 month between Oct, 31 2021 and Apr, 30 2021', function () {
       var result = differenceInMonths(
-        new Date(2021, 9 /* Oct */, 31),
-        new Date(2021, 3 /* Apr */, 30)
+        /* 1400/8/9 */ new Date(2021, 9 /* Oct */, 31),
+        /* 1400/2/10 */ new Date(2021, 3 /* Apr */, 30)
       )
       assert(result === 6)
     })
 
     it('it returns diff of -1 month between Sep, 30 2021 and Oct, 31 2021', function () {
       var result = differenceInMonths(
-        new Date(2021, 8 /* Sep */, 30),
-        new Date(2021, 9 /* Oct */, 31)
+        /* 1400/7/8 */ new Date(2021, 8 /* Sep */, 30),
+        /* 1400/8/9 */ new Date(2021, 9 /* Oct */, 31)
       )
       assert(result === -1)
     })
 
     it('the difference is less than a month, but the given dates are in different calendar months', function () {
       var result = differenceInMonths(
-        new Date(2014, 7 /* Aug */, 1),
-        new Date(2014, 6 /* Jul */, 31)
+        /* 1393/5/10 */ new Date(2014, 7 /* Aug */, 1),
+        /* 1393/5/9 */ new Date(2014, 6 /* Jul */, 31)
       )
       assert(result === 0)
     })
 
     it('the same for the swapped dates', function () {
       var result = differenceInMonths(
-        new Date(2014, 6 /* Jul */, 31),
-        new Date(2014, 7 /* Aug */, 1)
+        /* 1393/5/9 */ new Date(2014, 6 /* Jul */, 31),
+        /* 1393/5/10 */ new Date(2014, 7 /* Aug */, 1)
       )
       assert(result === 0)
     })
 
     it('the days of months of the given dates are the same', function () {
       var result = differenceInMonths(
-        new Date(2014, 8 /* Sep */, 6),
-        new Date(2014, 7 /* Aug */, 6)
+        /* 1393/6/15 */ new Date(2014, 8 /* Sep */, 6),
+        /* 1393/5/15 */ new Date(2014, 7 /* Aug */, 6)
       )
       assert(result === 1)
     })
 
     it('the given dates are the same', function () {
       var result = differenceInMonths(
-        new Date(2014, 8 /* Sep */, 5, 0, 0),
-        new Date(2014, 8 /* Sep */, 5, 0, 0)
+        /* 1393/6/14 */ new Date(2014, 8 /* Sep */, 5, 0, 0),
+        /* 1393/6/14 */ new Date(2014, 8 /* Sep */, 5, 0, 0)
       )
       assert(result === 0)
     })
@@ -116,8 +116,8 @@ describe('differenceInMonths', function () {
       }
 
       var result = differenceInMonths(
-        new Date(2014, 8 /* Sep */, 5, 0, 0),
-        new Date(2014, 8 /* Sep */, 5, 0, 0)
+        /* 1393/6/14 */ new Date(2014, 8 /* Sep */, 5, 0, 0),
+        /* 1393/6/14 */ new Date(2014, 8 /* Sep */, 5, 0, 0)
       )
 
       var resultIsNegative = isNegativeZero(result)
@@ -128,14 +128,14 @@ describe('differenceInMonths', function () {
   it('returns NaN if the first date is `Invalid Date`', function () {
     var result = differenceInMonths(
       new Date(NaN),
-      new Date(2017, 0 /* Jan */, 1)
+      /* 1395/10/12 */ new Date(2017, 0 /* Jan */, 1)
     )
     assert(isNaN(result))
   })
 
   it('returns NaN if the second date is `Invalid Date`', function () {
     var result = differenceInMonths(
-      new Date(2017, 0 /* Jan */, 1),
+      /* 1395/10/12 */ new Date(2017, 0 /* Jan */, 1),
       new Date(NaN)
     )
     assert(isNaN(result))
@@ -155,34 +155,34 @@ describe('differenceInMonths', function () {
     it('returns the number of full months between the given dates - end of Feb', function () {
       assert(
         differenceInMonths(
-          new Date(2012, 1 /* Feb */, 29, 9, 0, 0),
-          new Date(2012, 1 /* Feb */, 29, 10, 0, 0)
+          /* 1390/12/10 */ new Date(2012, 1 /* Feb */, 29, 9, 0, 0),
+          /* 1390/12/10 */ new Date(2012, 1 /* Feb */, 29, 10, 0, 0)
         ) === 0
       )
       assert(
         differenceInMonths(
-          new Date(2012, 1 /* Feb */, 28, 9, 0, 0),
-          new Date(2012, 1 /* Feb */, 29, 10, 0, 0)
+          /* 1390/12/9 */ new Date(2012, 1 /* Feb */, 28, 9, 0, 0),
+          /* 1390/12/10 */ new Date(2012, 1 /* Feb */, 29, 10, 0, 0)
         ) === 0
       )
       assert(
         differenceInMonths(
-          new Date(2012, 1 /* Feb */, 27, 9, 0, 0),
-          new Date(2012, 1 /* Feb */, 27, 10, 0, 0)
+          /* 1390/12/8 */ new Date(2012, 1 /* Feb */, 27, 9, 0, 0),
+          /* 1390/12/8 */ new Date(2012, 1 /* Feb */, 27, 10, 0, 0)
         ) === 0
       )
       assert(
         differenceInMonths(
-          new Date(2012, 1 /* Feb */, 28, 9, 0, 0),
-          new Date(2012, 1 /* Feb */, 28, 10, 0, 0)
+          /* 1390/12/9 */ new Date(2012, 1 /* Feb */, 28, 9, 0, 0),
+          /* 1390/12/9 */ new Date(2012, 1 /* Feb */, 28, 10, 0, 0)
         ) === 0
       )
     })
 
     assert(
       differenceInMonths(
-        new Date(2021, 1 /* Feb */, 28, 7, 23, 7),
-        new Date(2021, 1 /* Feb */, 28, 7, 38, 18)
+        /* 1399/12/10 */ new Date(2021, 1 /* Feb */, 28, 7, 23, 7),
+        /* 1399/12/10 */ new Date(2021, 1 /* Feb */, 28, 7, 38, 18)
       ) === 0
     )
   })
