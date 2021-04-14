52401e0f2f8261299ae63d2d4631792966eec701
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-04-14 14:17:27 +0430

    TYPES: update types

--- src/eachHourOfInterval/index.d.ts
+++ src/eachHourOfInterval/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { eachHourOfInterval } from 'date-fns'
+import { eachHourOfInterval } from 'date-fns-jalali'
 export default eachHourOfInterval
