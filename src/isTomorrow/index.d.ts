a617a5842aee1550ed248409b9fe698c1e765075
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-04-14 13:58:20 +0430

    TYPES: update types

--- src/isTomorrow/index.d.ts
+++ src/isTomorrow/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { isTomorrow } from 'date-fns'
+import { isTomorrow } from 'date-fns-jalali'
 export default isTomorrow
