ef91b1d1c33a79301965cecec6fa2a0204ac1b86
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-04-17 22:13:33 +0430

    TEST_COMMENTS: add comments to tests

--- src/nextWednesday/test.ts
+++ src/nextWednesday/test.ts
@@ -7,38 +7,38 @@ import nextWednesday from '.'
 describe('nextWednesday', () => {
   it('returns the following Wednesday given various dates before the same', () => {
     assert.deepStrictEqual(
-      nextWednesday(new Date(2020, 2 /* Mar */, 23)),
-      new Date(2020, 2 /* Mar */, 25)
+      nextWednesday(/* 1399/1/4 */ new Date(2020, 2 /* Mar */, 23)),
+      /* 1399/1/6 */ new Date(2020, 2 /* Mar */, 25)
     )
 
     assert.deepStrictEqual(
-      nextWednesday(new Date(2020, 2 /* Mar */, 22)),
-      new Date(2020, 2 /* Mar */, 25)
+      nextWednesday(/* 1399/1/3 */ new Date(2020, 2 /* Mar */, 22)),
+      /* 1399/1/6 */ new Date(2020, 2 /* Mar */, 25)
     )
 
     assert.deepStrictEqual(
-      nextWednesday(new Date(2020, 2 /* Mar */, 21)),
-      new Date(2020, 2 /* Mar */, 25)
+      nextWednesday(/* 1399/1/2 */ new Date(2020, 2 /* Mar */, 21)),
+      /* 1399/1/6 */ new Date(2020, 2 /* Mar */, 25)
     )
 
     assert.deepStrictEqual(
-      nextWednesday(new Date(2020, 2 /* Mar */, 20)),
-      new Date(2020, 2 /* Mar */, 25)
+      nextWednesday(/* 1399/1/1 */ new Date(2020, 2 /* Mar */, 20)),
+      /* 1399/1/6 */ new Date(2020, 2 /* Mar */, 25)
     )
 
     assert.deepStrictEqual(
-      nextWednesday(new Date(2020, 2 /* Mar */, 19)),
-      new Date(2020, 2 /* Mar */, 25)
+      nextWednesday(/* 1398/12/29 */ new Date(2020, 2 /* Mar */, 19)),
+      /* 1399/1/6 */ new Date(2020, 2 /* Mar */, 25)
     )
 
     assert.deepStrictEqual(
-      nextWednesday(new Date(2020, 2 /* Mar */, 18)),
-      new Date(2020, 2 /* Mar */, 25)
+      nextWednesday(/* 1398/12/28 */ new Date(2020, 2 /* Mar */, 18)),
+      /* 1399/1/6 */ new Date(2020, 2 /* Mar */, 25)
     )
 
     assert.deepStrictEqual(
-      nextWednesday(new Date(2020, 2 /* Mar */, 17)),
-      new Date(2020, 2 /* Mar */, 18)
+      nextWednesday(/* 1398/12/27 */ new Date(2020, 2 /* Mar */, 17)),
+      /* 1398/12/28 */ new Date(2020, 2 /* Mar */, 18)
     )
   })
 
