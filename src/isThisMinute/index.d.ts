3c8a5c588e379385c6780a993d95a6fb264492e6
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-04-17 22:13:10 +0430

    TYPES: update types

--- src/isThisMinute/index.d.ts
+++ src/isThisMinute/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { isThisMinute } from 'date-fns'
+import { isThisMinute } from 'date-fns-jalali'
 export default isThisMinute
