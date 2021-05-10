7ede03a6fa6c11aa4c8c9aaadcb2e5e4144421cc
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-05-10 16:07:38 +0430

    TYPES: update types

--- src/differenceInBusinessDays/index.d.ts
+++ src/differenceInBusinessDays/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { differenceInBusinessDays } from 'date-fns'
+import { differenceInBusinessDays } from 'date-fns-jalali'
 export default differenceInBusinessDays
