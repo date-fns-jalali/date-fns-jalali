5f07c79737520599ee719320dbaa6810e2aecf80
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-05-10 16:08:19 +0430

    TEST_COMMENTS: add comments to tests

--- src/getWeeksInMonth/test.ts
+++ src/getWeeksInMonth/test.ts
@@ -5,49 +5,63 @@ import getWeeksInMonth from '.'
 
 describe('getWeeksInMonth', function () {
   it('returns the number of calendar weeks the month in the given date spans', function () {
-    const result = getWeeksInMonth(new Date(2015, 1 /* Feb */, 8, 18, 0))
+    const result = getWeeksInMonth(
+      /* 1393/11/19 */ new Date(2015, 1 /* Feb */, 8, 18, 0)
+    )
     assert(result === 4)
   })
 
   it('allows to specify which day is the first day of the week', function () {
-    const result = getWeeksInMonth(new Date(2015, 1 /* Feb */, 8, 18, 0), {
-      weekStartsOn: 1,
-    })
+    const result = getWeeksInMonth(
+      /* 1393/11/19 */ new Date(2015, 1 /* Feb */, 8, 18, 0),
+      {
+        weekStartsOn: 1,
+      }
+    )
     assert(result === 5)
   })
 
   it('allows to specify which day is the first day of the week in locale', function () {
-    const result = getWeeksInMonth(new Date(2015, 1 /* Feb */, 8, 18, 0), {
-      // @ts-expect-error
-      locale: {
-        options: { weekStartsOn: 1 },
-      },
-    })
+    const result = getWeeksInMonth(
+      /* 1393/11/19 */ new Date(2015, 1 /* Feb */, 8, 18, 0),
+      {
+        // @ts-expect-error
+        locale: {
+          options: { weekStartsOn: 1 },
+        },
+      }
+    )
     assert(result === 5)
   })
 
   it('`options.weekStartsOn` overwrites the first day of the week specified in locale', function () {
-    const result = getWeeksInMonth(new Date(2015, 1 /* Feb */, 8, 18, 0), {
-      weekStartsOn: 1,
-      // @ts-expect-error
-      locale: {
-        options: { weekStartsOn: 0 },
-      },
-    })
+    const result = getWeeksInMonth(
+      /* 1393/11/19 */ new Date(2015, 1 /* Feb */, 8, 18, 0),
+      {
+        weekStartsOn: 1,
+        // @ts-expect-error
+        locale: {
+          options: { weekStartsOn: 0 },
+        },
+      }
+    )
     assert(result === 5)
   })
 
   it('accepts timestamps', function () {
     const result = getWeeksInMonth(
-      new Date(2017, 3 /* Apr */, 8, 18, 0).getTime()
+      /* 1396/1/19 */ new Date(2017, 3 /* Apr */, 8, 18, 0).getTime()
     )
     assert(result === 6)
   })
 
   it('does not mutate the original date', function () {
-    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
+    const date = /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
     getWeeksInMonth(date)
-    assert.deepStrictEqual(date, new Date(2014, 8 /* Sep */, 2, 11, 55, 0))
+    assert.deepStrictEqual(
+      date,
+      /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0)
+    )
   })
 
   it('returns NaN if the date is `Invalid Date`', function () {
@@ -58,7 +72,7 @@ describe('getWeeksInMonth', function () {
   it('throws `RangeError` if `options.weekStartsOn` is not convertable to 0, 1, ..., 6 or undefined', function () {
     const block = getWeeksInMonth.bind(
       null,
-      new Date(2014, 6 /* Jul */, 8, 18, 0),
+      /* 1393/4/17 */ new Date(2014, 6 /* Jul */, 8, 18, 0),
       { weekStartsOn: NaN }
     )
     assert.throws(block, RangeError)
--- src/nextSunday/test.ts
+++ src/nextSunday/test.ts
@@ -7,38 +7,38 @@ import nextSunday from '.'
 describe('nextSunday', () => {
   it('returns the following Sunday given various dates before the same', () => {
     assert.deepStrictEqual(
-      nextSunday(new Date(2020, 2 /* Mar */, 23)),
-      new Date(2020, 2 /* Mar */, 29)
+      nextSunday(/* 1399/1/4 */ new Date(2020, 2 /* Mar */, 23)),
+      /* 1399/1/10 */ new Date(2020, 2 /* Mar */, 29)
     )
 
     assert.deepStrictEqual(
-      nextSunday(new Date(2020, 2 /* Mar */, 22)),
-      new Date(2020, 2 /* Mar */, 29)
+      nextSunday(/* 1399/1/3 */ new Date(2020, 2 /* Mar */, 22)),
+      /* 1399/1/10 */ new Date(2020, 2 /* Mar */, 29)
     )
 
     assert.deepStrictEqual(
-      nextSunday(new Date(2020, 2 /* Mar */, 21)),
-      new Date(2020, 2 /* Mar */, 22)
+      nextSunday(/* 1399/1/2 */ new Date(2020, 2 /* Mar */, 21)),
+      /* 1399/1/3 */ new Date(2020, 2 /* Mar */, 22)
     )
 
     assert.deepStrictEqual(
-      nextSunday(new Date(2020, 2 /* Mar */, 20)),
-      new Date(2020, 2 /* Mar */, 22)
+      nextSunday(/* 1399/1/1 */ new Date(2020, 2 /* Mar */, 20)),
+      /* 1399/1/3 */ new Date(2020, 2 /* Mar */, 22)
     )
 
     assert.deepStrictEqual(
-      nextSunday(new Date(2020, 2 /* Mar */, 19)),
-      new Date(2020, 2 /* Mar */, 22)
+      nextSunday(/* 1398/12/29 */ new Date(2020, 2 /* Mar */, 19)),
+      /* 1399/1/3 */ new Date(2020, 2 /* Mar */, 22)
     )
 
     assert.deepStrictEqual(
-      nextSunday(new Date(2020, 2 /* Mar */, 18)),
-      new Date(2020, 2 /* Mar */, 22)
+      nextSunday(/* 1398/12/28 */ new Date(2020, 2 /* Mar */, 18)),
+      /* 1399/1/3 */ new Date(2020, 2 /* Mar */, 22)
     )
 
     assert.deepStrictEqual(
-      nextSunday(new Date(2020, 2 /* Mar */, 17)),
-      new Date(2020, 2 /* Mar */, 22)
+      nextSunday(/* 1398/12/27 */ new Date(2020, 2 /* Mar */, 17)),
+      /* 1399/1/3 */ new Date(2020, 2 /* Mar */, 22)
     )
   })
 
