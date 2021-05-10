c3a7dab72dc66ee7ad278f2114d8b980c2772fd5
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-05-10 19:56:34 +0430

    TYPES: update types

--- src/formatISODuration/index.d.ts
+++ src/formatISODuration/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { formatISODuration } from 'date-fns'
+import { formatISODuration } from 'date-fns-jalali'
 export default formatISODuration
--- src/fp/nextWednesday/index.d.ts
+++ src/fp/nextWednesday/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { nextWednesday } from 'date-fns/fp'
+import { nextWednesday } from 'date-fns-jalali/fp'
 export default nextWednesday
