7af4f1decb4ac51c9cfb3486d826eceacf9205ea
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-05-11 10:44:26 +0430

    TEST_COMMENTS: add comments to tests

--- src/set/test.ts
+++ src/set/test.ts
@@ -6,7 +6,7 @@ import set from '.'
 
 describe('set', function () {
   it('sets all values', function () {
-    const result = set(new Date(2013, 0 /* Jan */), {
+    const result = set(/* 1391/10/12 */ new Date(2013, 0 /* Jan */), {
       year: 2014,
       month: 8, // Sep
       date: 20,
@@ -17,107 +17,158 @@ describe('set', function () {
     })
     assert.deepEqual(
       result.toString(),
-      new Date(2014, 8 /* Sep */, 20, 12, 12, 12, 12).toString()
+      /* 1393/6/29 */ new Date(2014, 8 /* Sep */, 20, 12, 12, 12, 12).toString()
     )
   })
 
   it('sets year', function () {
-    const result = set(new Date(2013, 8 /* Sep */), { year: 2014 })
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */))
+    const result = set(/* 1392/6/10 */ new Date(2013, 8 /* Sep */), {
+      year: 2014,
+    })
+    assert.deepEqual(result, /* 1393/6/10 */ new Date(2014, 8 /* Sep */))
   })
 
   it('sets month', function () {
-    const result = set(new Date(2014, 8 /* Sep */), { month: 9 /* Oct */ })
-    assert.deepEqual(result, new Date(2014, 9 /* Oct */))
+    const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */), {
+      month: 9 /* Oct */,
+    })
+    assert.deepEqual(result, /* 1393/7/9 */ new Date(2014, 9 /* Oct */))
   })
 
   it('sets day of month', function () {
-    const result = set(new Date(2014, 8 /* Sep */), { date: 20 })
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 20))
+    const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */), {
+      date: 20,
+    })
+    assert.deepEqual(result, /* 1393/6/29 */ new Date(2014, 8 /* Sep */, 20))
   })
 
   it('sets hours', function () {
-    const result = set(new Date(2014, 8 /* Sep */, 1), { hours: 12 })
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 12))
+    const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1), {
+      hours: 12,
+    })
+    assert.deepEqual(result, /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1, 12))
   })
 
   it('sets minutes', function () {
-    const result = set(new Date(2014, 8 /* Sep */, 1, 1), { minutes: 12 })
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 1, 12))
+    const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1, 1), {
+      minutes: 12,
+    })
+    assert.deepEqual(
+      result,
+      /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1, 1, 12)
+    )
   })
 
   it('sets seconds', function () {
-    const result = set(new Date(2014, 8 /* Sep */, 1, 1, 1), { seconds: 12 })
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 1, 1, 12))
+    const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1, 1, 1), {
+      seconds: 12,
+    })
+    assert.deepEqual(
+      result,
+      /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1, 1, 1, 12)
+    )
   })
 
   it('sets milliseconds', function () {
-    const result = set(new Date(2014, 8 /* Sep */, 1, 1, 1, 1), {
-      milliseconds: 500,
-    })
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 1, 1, 1, 1, 500))
+    const result = set(
+      /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1, 1, 1, 1),
+      {
+        milliseconds: 500,
+      }
+    )
+    assert.deepEqual(
+      result,
+      /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1, 1, 1, 1, 500)
+    )
   })
 
   describe('value overflow', function () {
     it('months overflow into years', function () {
-      const result = set(new Date(2014, 8 /* Sep */, 1), {
+      const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1), {
         month: 12 /* 13th month */,
       })
-      assert.deepEqual(result, new Date(2015, 0 /* Jan */, 1))
+      assert.deepEqual(result, /* 1393/10/11 */ new Date(2015, 0 /* Jan */, 1))
     })
 
     it('days of months overflow into months', function () {
-      const result = set(new Date(2014, 8 /* Sep */, 1), { date: 31 })
-      assert.deepEqual(result, new Date(2014, 9 /* Oct */, 1))
+      const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1), {
+        date: 31,
+      })
+      assert.deepEqual(result, /* 1393/7/9 */ new Date(2014, 9 /* Oct */, 1))
     })
 
     it('hours overflow into days', function () {
-      const result = set(new Date(2014, 8 /* Sep */, 19), { hours: 24 })
-      assert.deepEqual(result, new Date(2014, 8 /* Sep */, 20))
+      const result = set(/* 1393/6/28 */ new Date(2014, 8 /* Sep */, 19), {
+        hours: 24,
+      })
+      assert.deepEqual(result, /* 1393/6/29 */ new Date(2014, 8 /* Sep */, 20))
     })
 
     it('minutes overflow into hours', function () {
-      const result = set(new Date(2014, 8 /* Sep */, 20, 11), { minutes: 60 })
-      assert.deepEqual(result, new Date(2014, 8 /* Sep */, 20, 12))
+      const result = set(/* 1393/6/29 */ new Date(2014, 8 /* Sep */, 20, 11), {
+        minutes: 60,
+      })
+      assert.deepEqual(
+        result,
+        /* 1393/6/29 */ new Date(2014, 8 /* Sep */, 20, 12)
+      )
     })
 
     it('seconds overflow into minutes', function () {
-      const result = set(new Date(2014, 8 /* Sep */, 20, 12, 58), {
-        seconds: 60,
-      })
-      assert.deepEqual(result, new Date(2014, 8 /* Sep */, 20, 12, 59))
+      const result = set(
+        /* 1393/6/29 */ new Date(2014, 8 /* Sep */, 20, 12, 58),
+        {
+          seconds: 60,
+        }
+      )
+      assert.deepEqual(
+        result,
+        /* 1393/6/29 */ new Date(2014, 8 /* Sep */, 20, 12, 59)
+      )
     })
 
     it('milliseconds overflow into seconds', function () {
-      const result = set(new Date(2014, 8 /* Sep */, 20, 12, 58, 30), {
-        milliseconds: 1000,
-      })
-      assert.deepEqual(result, new Date(2014, 8 /* Sep */, 20, 12, 58, 31))
+      const result = set(
+        /* 1393/6/29 */ new Date(2014, 8 /* Sep */, 20, 12, 58, 30),
+        {
+          milliseconds: 1000,
+        }
+      )
+      assert.deepEqual(
+        result,
+        /* 1393/6/29 */ new Date(2014, 8 /* Sep */, 20, 12, 58, 31)
+      )
     })
   })
 
   describe('edge cases', function () {
     it('sets January', function () {
-      const result = set(new Date(2014, 8 /* Sep */), { month: 0 /* Jan */ })
-      assert.deepEqual(result, new Date(2014, 0 /* Jan */))
+      const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */), {
+        month: 0 /* Jan */,
+      })
+      assert.deepEqual(result, /* 1392/10/11 */ new Date(2014, 0 /* Jan */))
     })
 
     it('sets the last day of new month if the initial date was the last day of a longer month', function () {
-      const result = set(new Date(2014, 7 /* Aug */, 31), {
+      const result = set(/* 1393/6/9 */ new Date(2014, 7 /* Aug */, 31), {
         month: 8 /* Sep */,
       })
-      assert.deepEqual(result, new Date(2014, 8 /* Sep */, 30))
+      assert.deepEqual(result, /* 1393/7/8 */ new Date(2014, 8 /* Sep */, 30))
     })
 
     it('ignores undefined values', function () {
-      const result = set(new Date(2014, 8 /* Sep */), { year: undefined })
-      assert.deepEqual(result, new Date(2014, 8 /* Sep */))
+      const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */), {
+        year: undefined,
+      })
+      assert.deepEqual(result, /* 1393/6/10 */ new Date(2014, 8 /* Sep */))
     })
 
     it('ignores null values', function () {
       // @ts-expect-error
-      const result = set(new Date(2014, 8 /* Sep */), { year: null })
-      assert.deepEqual(result, new Date(2014, 8 /* Sep */))
+      const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */), {
+        year: null,
+      })
+      assert.deepEqual(result, /* 1393/6/10 */ new Date(2014, 8 /* Sep */))
     })
 
     it('throws TypeError exception if passed less than 2 arguments', function () {
@@ -125,7 +176,9 @@ describe('set', function () {
     })
 
     it('returns Invalid Date if any value in values is NaN', function () {
-      const result = set(new Date(2014, 8 /* Sep */), { year: NaN })
+      const result = set(/* 1393/6/10 */ new Date(2014, 8 /* Sep */), {
+        year: NaN,
+      })
       assert.deepEqual(isNaN(result.getTime()), isNaN(new Date(NaN).getTime()))
     })
 
