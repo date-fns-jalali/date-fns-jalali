dbd36f6f948451cb2ae3e37e02e27ffc017063a1
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-05-30 18:01:23 +0430

    TYPES: update types

--- src/fp/eachYearOfInterval/index.d.ts
+++ src/fp/eachYearOfInterval/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { eachYearOfInterval } from 'date-fns/fp'
+import { eachYearOfInterval } from 'date-fns-jalali/fp'
 export default eachYearOfInterval
