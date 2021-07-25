8192418532c5ad137c5d459a413ea995eae9325a
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-07-25 12:24:50 +0430

    TYPES: update types

--- src/fp/parse/index.d.ts
+++ src/fp/parse/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { parse } from 'date-fns/fp'
+import { parse } from 'date-fns-jalali/fp'
 export default parse
--- src/quartersToYears/index.d.ts
+++ src/quartersToYears/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { quartersToYears } from 'date-fns'
+import { quartersToYears } from 'date-fns-jalali'
 export default quartersToYears
