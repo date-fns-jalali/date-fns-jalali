f933c61d58094c8e273873a92d14ad98dd21a843
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-07-25 12:22:00 +0430

    USE_CORE: use core functions

--- src/_lib/startOfUTCWeekYear/index.js
+++ src/_lib/startOfUTCWeekYear/index.js
@@ -3,6 +3,9 @@ import getUTCWeekYear from '../getUTCWeekYear/index'
 import startOfUTCWeek from '../startOfUTCWeek/index'
 import requiredArgs from '../requiredArgs/index'
 
+import coreSetUTCFullYear from '../../_core/setUTCFullYear/index'
+import coreNewDate from '../../_core/newDate/index'
+
 // This function will be a part of public API when UTC function will be implemented.
 // See issue: https://github.com/date-fns/date-fns/issues/376
 export default function startOfUTCWeekYear(dirtyDate, dirtyOptions) {
@@ -22,8 +25,8 @@ export default function startOfUTCWeekYear(dirtyDate, dirtyOptions) {
       : toInteger(options.firstWeekContainsDate)
 
   var year = getUTCWeekYear(dirtyDate, dirtyOptions)
-  var firstWeek = new Date(0)
-  firstWeek.setUTCFullYear(year, 0, firstWeekContainsDate)
+  var firstWeek = coreNewDate(0)
+  coreSetUTCFullYear(firstWeek, year, 0, firstWeekContainsDate)
   firstWeek.setUTCHours(0, 0, 0, 0)
   var date = startOfUTCWeek(firstWeek, dirtyOptions)
   return date
