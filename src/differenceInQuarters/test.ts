909fbd4010f551e3df17a8ae4a1fefc3df5074a6
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-09-18 15:51:12 +0430

    fixup! TEST: fix tests

--- src/differenceInQuarters/test.ts
+++ src/differenceInQuarters/test.ts
@@ -7,7 +7,7 @@ describe('differenceInQuarters', () => {
   it('returns the number of full quarters between the given dates  with `trunc` as a default rounding method', () => {
     const result = differenceInQuarters(
       /* 1391/4/12 */ new Date(2012, 6 /* Jul */, 2, 5, 0),
-      /* 1390/4/11 */ new Date(2011, 6 /* Jul */, 2, 6, 0)
+      /* 1390/4/12 */ new Date(2011, 6 /* Jul */, 3, 6, 0)
     )
     assert(result === 3)
   })
