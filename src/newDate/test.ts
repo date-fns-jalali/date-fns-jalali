12ce2de1ef3dee1a745e86f19a9dca1542a0db49
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-10-06 05:55:26 -0400

    TEST_COMMENTS: add comments to tests

--- src/newDate/test.ts
+++ src/newDate/test.ts
@@ -7,10 +7,13 @@ import newDate from '.'
 describe('newDate', function () {
   it('create a date', function () {
     const result = newDate(2014, 8 /* Sep */, 30)
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 30))
+    assert.deepEqual(result, /* 1393/7/8 */ new Date(2014, 8 /* Sep */, 30))
   })
   it('create a date with time', function () {
     const result = newDate(2014, 8 /* Sep */, 30, 20, 40)
-    assert.deepEqual(result, new Date(2014, 8 /* Sep */, 30, 20, 40))
+    assert.deepEqual(
+      result,
+      /* 1393/7/8 */ new Date(2014, 8 /* Sep */, 30, 20, 40)
+    )
   })
 })
