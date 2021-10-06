527ea0c6648c4ed47ce03285820fe00afaa6182c
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-10-06 05:27:55 -0400

    TYPES: update types

--- src/fp/previousMonday/index.d.ts
+++ src/fp/previousMonday/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { previousMonday } from 'date-fns/fp'
+import { previousMonday } from 'date-fns-jalali/fp'
 export default previousMonday
