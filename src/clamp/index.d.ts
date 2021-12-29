334975392fad773ce575bb4acf3234ff4227741d
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-12-29 12:12:13 +0330

    TYPES: update types

--- src/clamp/index.d.ts
+++ src/clamp/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { clamp } from 'date-fns'
+import { clamp } from 'date-fns-jalali'
 export default clamp
