13f5302d67d707c122c141fef288aa282800816b
Sasha Koss <koss@nocorp.me>
2022-06-11 12:29:57 +0800

    (utc): Add setters and some of the formatters

--- index.ts
+++ index.ts
@@ -1,14 +1,3 @@
-const getMethods: Array<keyof Date> = [
-  "getDate",
-  "getDay",
-  "getFullYear",
-  "getHours",
-  "getMilliseconds",
-  "getMinutes",
-  "getMonth",
-  "getSeconds",
-];
-
 export class UTCDate extends Date {
   constructor();
 
@@ -39,5 +28,69 @@ export class UTCDate extends Date {
         return this[utcMethod](...args);
       };
     }
+
+    for (const method of setMethods) {
+      // @ts-ignore - how to type overload?
+      this[method] = (...args) => {
+        const utcMethod = method
+          .toString()
+          .replace("set", "setUTC") as keyof Date;
+        // @ts-ignore - how to call the UTC method?
+        return this[utcMethod](...args);
+      };
+    }
+  }
+
+  toString(): string {
+    const weekday = weekdayFormat.format(this);
+    const date = dateFormat.format(this);
+    const year = this.getFullYear();
+    const time = this.toTimeString();
+    return `${weekday} ${date} ${year} ${time}`;
+  }
+
+  toTimeString(): string {
+    const time = timeFormat.format(this);
+    return `${time} GMT+0000 (Coordinated Universal Time)`;
   }
 }
+
+var getMethods: Array<keyof Date> = [
+  "getDate",
+  "getDay",
+  "getFullYear",
+  "getHours",
+  "getMilliseconds",
+  "getMinutes",
+  "getMonth",
+  "getSeconds",
+];
+
+var setMethods: Array<keyof Date> = [
+  "setDate",
+  "setFullYear",
+  "setHours",
+  "setMilliseconds",
+  "setMinutes",
+  "setMonth",
+  "setSeconds",
+];
+
+var weekdayFormat = new Intl.DateTimeFormat("en-US", {
+  weekday: "short",
+  timeZone: "UTC",
+});
+
+var dateFormat = new Intl.DateTimeFormat("en-US", {
+  month: "short",
+  day: "numeric",
+  timeZone: "UTC",
+});
+
+var timeFormat = new Intl.DateTimeFormat("en-US", {
+  hour12: false,
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+  timeZone: "UTC",
+});
--- test.ts
+++ test.ts
@@ -51,4 +51,68 @@ describe("UTCDate", () => {
   describe("getSeconds", () => {
     it.todo("returns UTC seconds");
   });
+
+  describe("setDate", () => {
+    it("sets UTC date", () => {
+      const date = new UTCDate(1987, 1, 11, 23);
+      date.setDate(11);
+      expect(date.getDate()).toBe(11);
+    });
+  });
+
+  describe("setFullYear", () => {
+    it("sets UTC full year", () => {
+      const date = new UTCDate(1999, 11, 31, 23);
+      date.setFullYear(1999);
+      expect(date.getFullYear()).toBe(1999);
+    });
+  });
+
+  describe("setHours", () => {
+    it("sets UTC hours", () => {
+      const date = new UTCDate(1987, 1, 11, 3, 30);
+      date.setHours(4);
+      expect(date.getHours()).toBe(4);
+    });
+  });
+
+  describe("setMilliseconds", () => {
+    it.todo("sets UTC milliseconds");
+  });
+
+  describe("setMinutes", () => {
+    it("sets UTC minutes", () => {
+      const date = new UTCDate(1987, 1, 11, 3, 30);
+      date.setMinutes(0);
+      expect(date.getMinutes()).toBe(0);
+    });
+  });
+
+  describe("setMonth", () => {
+    it("sets UTC months", () => {
+      const date = new UTCDate(1999, 11, 31, 23);
+      date.setMonth(11);
+      expect(date.getMonth()).toBe(11);
+    });
+  });
+
+  describe("setSeconds", () => {
+    it.todo("sets UTC seconds");
+  });
+
+  describe("toString", () => {
+    it("returns string representing the given date in UTC timezone", () => {
+      expect(new UTCDate(1987, 1, 11, 12, 13, 14, 15).toString()).toBe(
+        "Wed Feb 11 1987 12:13:14 GMT+0000 (Coordinated Universal Time)"
+      );
+    });
+  });
+
+  describe("toTimeString", () => {
+    it("returns string representing the given time in UTC timezone", () => {
+      expect(new UTCDate(1987, 1, 11, 12, 13, 14, 15).toTimeString()).toBe(
+        "12:13:14 GMT+0000 (Coordinated Universal Time)"
+      );
+    });
+  });
 });
