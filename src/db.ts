903056d60b99266e8468914470a6730dde32b824
Sasha Koss <koss@nocorp.me>
2022-12-27 14:45:49 +0800

    (docs): Import JSDoc page types

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { JSONBond } from "json-bond";
+import type { ContainerReflection } from "typedoc";
 import { schema, Typesaurus } from "typesaurus";
-import type { ContainerReflection, ReflectionKind } from "typedoc";
 
 export const db = schema(($) => ({
   packages: $.collection<Package>(),
@@ -9,6 +9,8 @@ export const db = schema(($) => ({
 }));
 
 export type Schema = Typesaurus.Schema<typeof db>;
+
+// export const sub
 
 export interface Package {
   name: string;
@@ -34,7 +36,7 @@ export interface PagePreview {
   summary: string;
 }
 
-export type Page = TsDocPage | MarkdownPage;
+export type Page = MarkdownPage | TSDocPage | JSDocPage;
 
 export interface PageBase {
   package: string;
@@ -45,13 +47,109 @@ export interface PageBase {
   summary: string;
 }
 
-export interface TsDocPage extends PageBase {
+/**
+ * Generic Markdown page (used for v1, v2 and v3 documentation).
+ */
+export interface MarkdownPage extends PageBase {
+  type: "markdown";
+  markdown: string;
+}
+
+/**
+ * TSDoc function page (v3).
+ */
+export interface TSDocPage extends PageBase {
   type: "tsdoc";
   name: string;
   tsdoc: JSONBond<ContainerReflection>;
 }
 
-export interface MarkdownPage extends PageBase {
-  type: "markdown";
-  markdown: string;
+/**
+ * JSDoc function page (used for v1 and v2 documentation).
+ */
+export interface JSDocPage {
+  type: "jsdoc";
+  name: string;
+  doc: JSONBond<JSDocFunction>;
+}
+
+/**
+ * JSDoc function definition (used for v1 and v2 documentation).
+ */
+export interface JSDocFunction {
+  args?: JSDocParam[];
+  category: string;
+  content: {
+    category: string;
+    description: string;
+    examples?: string | string[];
+    exceptions: JSDocAttribute[];
+    id: string;
+    kind: string;
+    longname: string;
+    meta: {
+      filename: string;
+      lineno: number;
+      path: string;
+    };
+    name: string;
+    order: number;
+    properties?: JSDocParam[];
+    params?: JSDocParam[];
+    returns?: JSDocAttribute[];
+    scope: string;
+    summary: string;
+    type?: JSDocType;
+  };
+  description: string;
+  isFPFn?: boolean;
+  kind: "function" | "typedef";
+  relatedDocs?: {
+    default?: string;
+    fp?: string;
+    fpWithOptions?: string;
+  };
+  syntax?: string;
+  title: string;
+  type: "jsdoc";
+  urlId: string;
+  usage?: JSDocUsage;
+  usageTabs?: string[];
+}
+
+/**
+ * JSDoc type, used to define function arguments and return values.
+ */
+export interface JSDocType {
+  names: string[];
+}
+
+/**
+ * JSDoc attribute (return, exception, base for param).
+ */
+export interface JSDocAttribute {
+  description: string;
+  type: JSDocType;
+}
+
+/**
+ * JSDoc param.
+ */
+export interface JSDocParam extends JSDocAttribute {
+  name: string;
+  optional?: boolean;
+  defaultvalue?: string;
+  variable?: boolean;
+  props?: JSDocParam[];
+}
+
+/**
+ * JSDoc usage map.
+ */
+export interface JSDocUsage {
+  [usageTab: string]: {
+    code: string;
+    title: string;
+    text?: string;
+  };
 }
