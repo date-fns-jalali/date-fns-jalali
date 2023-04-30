94e844d881d54397176d3c21112143ebf7d9cbfa
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2023-04-30 22:58:46 +0330

    TYPES: update types

--- src/fp/previousTuesday/index.d.ts
+++ src/fp/previousTuesday/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { previousTuesday } from 'date-fns/fp'
+import { previousTuesday } from 'date-fns-jalali/fp'
 export default previousTuesday
--- src/sub/index.d.ts
+++ src/sub/index.d.ts
@@ -1,4 +1,4 @@
 // This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
 
-import { sub } from 'date-fns'
+import { sub } from 'date-fns-jalali'
 export default sub
