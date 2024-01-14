1cedc88ba07db8e70bee236b0f8634a1af52193c
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-01-14 07:50:37 +0330

    fixup! CORE: Add core functions

--- src/_core/setDate/index.ts
+++ src/_core/setDate/index.ts
@@ -4,9 +4,6 @@
  * @param args
  * @returns {number}
  */
-export function setDate(
-  cleanDate: Date,
-  ...args: Parameters<Date["setDate"]>
-) {
+export function setDate(cleanDate: Date, ...args: Parameters<Date["setDate"]>) {
   return cleanDate.setDate(...args);
 }
