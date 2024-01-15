8722d0048b76985aa8f8a601a76866887e51e424
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-01-15 23:46:04 +0330

    TEST_COMMENTS: add comments to tests

--- src/startOfWeek/test.ts
+++ src/startOfWeek/test.ts
@@ -6,79 +6,116 @@ import { startOfWeek } from "./index.js";
 
 describe("startOfWeek", () => {
   it("returns the date with the time set to 00:00:00 and the date set to the first day of a week", () => {
-    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
+    const date = /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
     const result = startOfWeek(date);
-    assert.deepStrictEqual(result, new Date(2014, 7 /* Aug */, 31));
+    assert.deepStrictEqual(
+      result,
+      /* 1393/6/9 */ new Date(2014, 7 /* Aug */, 31),
+    );
   });
 
   it("allows to specify which day is the first day of the week", () => {
-    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
+    const date = /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
     const result = startOfWeek(date, { weekStartsOn: 1 });
-    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 1));
+    assert.deepStrictEqual(
+      result,
+      /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+    );
   });
 
   it("allows to specify which day is the first day of the week in locale", () => {
-    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
+    const date = /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
     const result = startOfWeek(date, {
       locale: {
         options: { weekStartsOn: 1 },
       },
     });
-    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 1));
+    assert.deepStrictEqual(
+      result,
+      /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+    );
   });
 
   it("`options.weekStartsOn` overwrites the first day of the week specified in locale", () => {
-    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
+    const date = /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
     const result = startOfWeek(date, {
       weekStartsOn: 1,
       locale: {
         options: { weekStartsOn: 0 },
       },
     });
-    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 1));
+    assert.deepStrictEqual(
+      result,
+      /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+    );
   });
 
   it("accepts a timestamp", () => {
-    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0).getTime();
+    const date = /* 1393/6/11 */ new Date(
+      2014,
+      8 /* Sep */,
+      2,
+      11,
+      55,
+      0,
+    ).getTime();
     const result = startOfWeek(date);
-    assert.deepStrictEqual(result, new Date(2014, 7 /* Aug */, 31));
+    assert.deepStrictEqual(
+      result,
+      /* 1393/6/9 */ new Date(2014, 7 /* Aug */, 31),
+    );
   });
 
   it("does not mutate the original date", () => {
-    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
+    const date = /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
     startOfWeek(date);
-    assert.deepStrictEqual(date, new Date(2014, 8 /* Sep */, 2, 11, 55, 0));
+    assert.deepStrictEqual(
+      date,
+      /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0),
+    );
   });
 
   describe("edge cases", () => {
     describe("when the given day is before the start of a week", () => {
       it("it returns the start of a week", () => {
-        const date = new Date(2014, 9 /* Oct */, 6);
+        const date = /* 1393/7/14 */ new Date(2014, 9 /* Oct */, 6);
         const result = startOfWeek(date, { weekStartsOn: 3 });
-        assert.deepStrictEqual(result, new Date(2014, 9 /* Oct */, 1));
+        assert.deepStrictEqual(
+          result,
+          /* 1393/7/9 */ new Date(2014, 9 /* Oct */, 1),
+        );
       });
     });
 
     describe("when the given day is the start of a week", () => {
       it("it returns the start of a week", () => {
-        const date = new Date(2014, 9 /* Oct */, 8);
+        const date = /* 1393/7/16 */ new Date(2014, 9 /* Oct */, 8);
         const result = startOfWeek(date, { weekStartsOn: 3 });
-        assert.deepStrictEqual(result, new Date(2014, 9 /* Oct */, 8));
+        assert.deepStrictEqual(
+          result,
+          /* 1393/7/16 */ new Date(2014, 9 /* Oct */, 8),
+        );
       });
     });
 
     describe("when the given day is after the start of a week", () => {
       it("it returns the start of a week", () => {
-        const date = new Date(2014, 9 /* Oct */, 10);
+        const date = /* 1393/7/18 */ new Date(2014, 9 /* Oct */, 10);
         const result = startOfWeek(date, { weekStartsOn: 3 });
-        assert.deepStrictEqual(result, new Date(2014, 9 /* Oct */, 8));
+        assert.deepStrictEqual(
+          result,
+          /* 1393/7/16 */ new Date(2014, 9 /* Oct */, 8),
+        );
       });
     });
 
     it("handles the week at the start of a year", () => {
-      const date = new Date(2014, 0 /* Jan */, 1);
+      const date = /* 1392/10/11 */ new Date(2014, 0 /* Jan */, 1);
       const result = startOfWeek(date);
-      assert.deepStrictEqual(result, new Date(2013, 11 /* Dec */, 29));
+      assert.deepStrictEqual(
+        result,
+        /* 1392/10/8 */ new Date(2013, 11 /* Dec */, 29),
+      );
     });
   });
 
