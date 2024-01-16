86a16e8bf5f8acd7471ef094b266783bb7fab224
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-01-16 07:07:52 +0330

    TEST_COMMENTS: add comments to tests

--- src/isWithinInterval/test.ts
+++ src/isWithinInterval/test.ts
@@ -6,90 +6,120 @@ import { isWithinInterval } from "./index.js";
 
 describe("isWithinInterval", () => {
   it("returns true if the given date in within the given interval", () => {
-    const result = isWithinInterval(new Date(2014, 9 /* Oct */, 31), {
-      start: new Date(2014, 8 /* Sep */, 1),
-      end: new Date(2014, 11 /* Dec */, 31),
-    });
+    const result = isWithinInterval(
+      /* 1393/8/9 */ new Date(2014, 9 /* Oct */, 31),
+      {
+        start: /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+        end: /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
+      },
+    );
     assert(result === true);
   });
 
   it("returns true if the given date has same time as the left boundary of the interval", () => {
-    const result = isWithinInterval(new Date(2014, 8 /* Sep */, 1), {
-      start: new Date(2014, 8 /* Sep */, 1),
-      end: new Date(2014, 11 /* Dec */, 31),
-    });
+    const result = isWithinInterval(
+      /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+      {
+        start: /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+        end: /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
+      },
+    );
     assert(result === true);
   });
 
   it("returns true if the given date has same time as the right boundary of the interval", () => {
-    const result = isWithinInterval(new Date(2014, 11 /* Dec */, 31), {
-      start: new Date(2014, 8 /* Sep */, 1),
-      end: new Date(2014, 11 /* Dec */, 31),
-    });
+    const result = isWithinInterval(
+      /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
+      {
+        start: /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+        end: /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
+      },
+    );
     assert(result === true);
   });
 
   it("returns true if the given date and the both boundaries are the same", () => {
-    const result = isWithinInterval(new Date(2014, 11 /* Dec */, 31), {
-      start: new Date(2014, 11 /* Dec */, 31),
-      end: new Date(2014, 11 /* Dec */, 31),
-    });
+    const result = isWithinInterval(
+      /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
+      {
+        start: /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
+        end: /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
+      },
+    );
     assert(result === true);
   });
 
   it("returns false if the given date is outside of the interval", () => {
-    const result = isWithinInterval(new Date(2014, 1 /* Feb */, 11), {
-      start: new Date(2014, 8 /* Sep */, 1),
-      end: new Date(2014, 11 /* Dec */, 31),
-    });
+    const result = isWithinInterval(
+      /* 1392/11/22 */ new Date(2014, 1 /* Feb */, 11),
+      {
+        start: /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+        end: /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
+      },
+    );
     assert(result === false);
   });
 
   it("accepts a timestamp", () => {
-    const result = isWithinInterval(new Date(2014, 9 /* Oct */, 31).getTime(), {
-      start: new Date(2014, 8 /* Sep */, 1).getTime(),
-      end: new Date(2014, 11 /* Dec */, 31).getTime(),
-    });
+    const result = isWithinInterval(
+      /* 1393/8/9 */ new Date(2014, 9 /* Oct */, 31).getTime(),
+      {
+        start: /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1).getTime(),
+        end: /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31).getTime(),
+      },
+    );
     assert(result === true);
   });
 
   it("normalizes the interval if the start date is after the end date", () => {
-    const result = isWithinInterval(new Date(2014, 9 /* Oct */, 31), {
-      start: new Date(2014, 11 /* Dec */, 31),
-      end: new Date(2014, 8 /* Sep */, 1),
-    });
+    const result = isWithinInterval(
+      /* 1393/8/9 */ new Date(2014, 9 /* Oct */, 31),
+      {
+        start: /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
+        end: /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+      },
+    );
     assert(result === true);
   });
 
   it("returns false if the given date is `Invalid Date`", () => {
     const result = isWithinInterval(new Date(NaN), {
-      start: new Date(2014, 8 /* Sep */, 1),
-      end: new Date(2014, 11 /* Dec */, 31),
+      start: /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+      end: /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
     });
     assert(!result);
   });
 
   it("returns false if the start date is `Invalid Date`", () => {
-    const result = isWithinInterval(new Date(2014, 9 /* Oct */, 31), {
-      start: new Date(NaN),
-      end: new Date(2014, 8 /* Sep */, 1),
-    });
+    const result = isWithinInterval(
+      /* 1393/8/9 */ new Date(2014, 9 /* Oct */, 31),
+      {
+        start: new Date(NaN),
+        end: /* 1393/6/10 */ new Date(2014, 8 /* Sep */, 1),
+      },
+    );
     assert(!result);
   });
 
   it("returns false if the end date is `Invalid Date`", () => {
-    const result = isWithinInterval(new Date(2014, 9 /* Oct */, 31), {
-      start: new Date(2014, 11 /* Dec */, 31),
-      end: new Date(NaN),
-    });
+    const result = isWithinInterval(
+      /* 1393/8/9 */ new Date(2014, 9 /* Oct */, 31),
+      {
+        start: /* 1393/10/10 */ new Date(2014, 11 /* Dec */, 31),
+        end: new Date(NaN),
+      },
+    );
     assert(!result);
   });
 
   it("properly sorts the dates", () => {
-    const result = isWithinInterval(new Date(2023, 11 /* Dec */, 19), {
-      start: new Date(2001, 8 /* Sep */, 1),
-      end: new Date(2023, 11 /* Dec */, 20),
-    });
+    const result = isWithinInterval(
+      /* 1402/9/28 */ new Date(2023, 11 /* Dec */, 19),
+      {
+        start: /* 1380/6/10 */ new Date(2001, 8 /* Sep */, 1),
+        end: /* 1402/9/29 */ new Date(2023, 11 /* Dec */, 20),
+      },
+    );
     assert(result);
   });
 });
--- src/startOfDecade/test.ts
+++ src/startOfDecade/test.ts
@@ -6,21 +6,30 @@ import { startOfDecade } from "./index.js";
 
 describe("startOfDecade", () => {
   it("returns the date with the time set to 00:00:00 and the date set to the first day of a year", () => {
-    const date = new Date(1953, 3 /* Apr */, 13);
+    const date = /* 1332/1/24 */ new Date(1953, 3 /* Apr */, 13);
     const result = startOfDecade(date);
-    assert.deepStrictEqual(result, new Date(1950, 0 /* Jan */, 1));
+    assert.deepStrictEqual(
+      result,
+      /* 1328/10/11 */ new Date(1950, 0 /* Jan */, 1),
+    );
   });
 
   it("accepts a timestamp", () => {
-    const date = new Date(1984, 9 /* Oct */, 14).getTime();
+    const date = /* 1363/7/22 */ new Date(1984, 9 /* Oct */, 14).getTime();
     const result = startOfDecade(date);
-    assert.deepStrictEqual(result, new Date(1980, 0 /* Jan */, 1));
+    assert.deepStrictEqual(
+      result,
+      /* 1358/10/11 */ new Date(1980, 0 /* Jan */, 1),
+    );
   });
 
   it("does not mutate the original date", () => {
-    const date = new Date(1978, 10 /* Nov */, 14);
+    const date = /* 1357/8/23 */ new Date(1978, 10 /* Nov */, 14);
     startOfDecade(date);
-    assert.deepStrictEqual(date, new Date(1978, 10 /* Nov */, 14));
+    assert.deepStrictEqual(
+      date,
+      /* 1357/8/23 */ new Date(1978, 10 /* Nov */, 14),
+    );
   });
 
   it("returns `Invalid Date` if the given date is invalid", () => {
