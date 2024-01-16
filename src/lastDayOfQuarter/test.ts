45ac5f29deb0bd9afe8c7df2984d71c2fef8c099
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-01-16 07:34:48 +0330

    TEST_COMMENTS: add comments to tests

--- src/lastDayOfQuarter/test.ts
+++ src/lastDayOfQuarter/test.ts
@@ -6,21 +6,37 @@ import { lastDayOfQuarter } from "./index.js";
 
 describe("lastDayOfQuarter", () => {
   it("returns the date with the time set to 00:00:00 and the date set to the last day of a quarter", () => {
-    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
+    const date = /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
     const result = lastDayOfQuarter(date);
-    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 30));
+    assert.deepStrictEqual(
+      result,
+      /* 1393/7/8 */ new Date(2014, 8 /* Sep */, 30),
+    );
   });
 
   it("accepts a timestamp", () => {
-    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0).getTime();
+    const date = /* 1393/6/11 */ new Date(
+      2014,
+      8 /* Sep */,
+      2,
+      11,
+      55,
+      0,
+    ).getTime();
     const result = lastDayOfQuarter(date);
-    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 30));
+    assert.deepStrictEqual(
+      result,
+      /* 1393/7/8 */ new Date(2014, 8 /* Sep */, 30),
+    );
   });
 
   it("does not mutate the original date", () => {
-    const date = new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
+    const date = /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0);
     lastDayOfQuarter(date);
-    assert.deepStrictEqual(date, new Date(2014, 8 /* Sep */, 2, 11, 55, 0));
+    assert.deepStrictEqual(
+      date,
+      /* 1393/6/11 */ new Date(2014, 8 /* Sep */, 2, 11, 55, 0),
+    );
   });
 
   it("returns `Invalid Date` if the given date is invalid", () => {
