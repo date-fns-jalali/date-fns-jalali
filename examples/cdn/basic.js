f1ecd74d3deab831b23fa8a824b3e5b1c7c4d0c4
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-01-19 13:14:49 +0330

    ITEST: fix integration tests

--- examples/cdn/basic.js
+++ examples/cdn/basic.js
@@ -2,7 +2,7 @@ import { testScript } from "./dom.js";
 
 testScript("cdn.min.js", (dom) => {
   const result = dom.window.eval(
-    `window.dateFns.addDays(new Date(1987, 1, 11), 1).getDate()`,
+    `window.dateFnsJalali.addDays(new Date(1987, 1, 11), 1).getDate()`,
   );
   console.log(result === 12);
 });
