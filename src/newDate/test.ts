720ad2cf106ceb1b35248668964a785977ba17b4
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-03-19 13:33:57 +0330

    fixup! NEW_DATE: add newDate function

--- src/newDate/test.ts
+++ src/newDate/test.ts
@@ -1,16 +1,13 @@
-/* eslint-env mocha */
-
-import assert from "assert";
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { newDate } from "./index.js";
 
 describe("newDate", function () {
   it("create a date", function () {
     const result = newDate(2014, 8 /* Sep */, 30);
-    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 30));
+    expect(result).toEqual(new Date(2014, 8 /* Sep */, 30));
   });
   it("create a date with time", function () {
     const result = newDate(2014, 8 /* Sep */, 30, 20, 40);
-    assert.deepStrictEqual(result, new Date(2014, 8 /* Sep */, 30, 20, 40));
+    expect(result).toEqual(new Date(2014, 8 /* Sep */, 30, 20, 40));
   });
 });
