759bcef22f36dea117d1144c8899c982ef1e8cc1
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-03-19 13:39:35 +0330

    fixup! JALALI: add jalali functions

--- src/_lib/jalali/test.ts
+++ src/_lib/jalali/test.ts
@@ -5,8 +5,7 @@
 // j2d(y, 1, 1) - j2d(y + 1, 1, 1) =>   365  | 366
 // j2d(y, 12, 30) - j2d(y + 1, 1, 1) => 0    | 1
 
-import assert from "assert";
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import {
   d2g,
   d2j,
@@ -42,7 +41,7 @@ describe("j2d/d2j", () => {
     for (let i = START; i < END; i++) {
       const { jy, jm, jd } = d2j(i);
       const j = j2d(jy, jm, jd);
-      assert.strictEqual(j, i, `i=${i}, j=${j}, jy=${jy}, jm=${jm}, jd=${jd}`);
+      expect(j).toBe(i);
     }
   });
   it("each day of year should be one from tomorrow", () => {
@@ -50,7 +49,7 @@ describe("j2d/d2j", () => {
       let last_day = j2d(y, 1, 0);
       for (const [m, d] of JALALI_YEAR_DAYS) {
         const j = j2d(y, m, d);
-        assert.strictEqual(j - last_day, 1);
+        expect(j - last_day).toBe(1);
         last_day = j;
       }
     }
@@ -62,33 +61,33 @@ describe("j2d/d2j", () => {
       const last_day = j2d(y, 12, 30);
       const next_day = j2d(y + 1, 1, 1);
       if (is_leap) {
-        assert.strictEqual(next_day - first_day, 366, `y=${y}`);
-        assert.strictEqual(next_day - last_day, 1);
+        expect(next_day - first_day).toBe(366);
+        expect(next_day - last_day).toBe(1);
       } else {
-        assert.strictEqual(next_day - first_day, 365);
-        assert.strictEqual(next_day - last_day, 0);
+        expect(next_day - first_day).toBe(365);
+        expect(next_day - last_day).toBe(0);
       }
     }
   });
   it("should handle negative/overflow month", () => {
     for (let m = 1; m <= 12; m++) {
-      assert.deepStrictEqual(d2j(j2d(1400, m + 24, 1)), {
+      expect(d2j(j2d(1400, m + 24, 1))).toEqual({
         jy: 1402,
         jm: m,
         jd: 1,
       });
-      assert.deepStrictEqual(d2j(j2d(1400, m + 12, 1)), {
+      expect(d2j(j2d(1400, m + 12, 1))).toEqual({
         jy: 1401,
         jm: m,
         jd: 1,
       });
-      assert.deepStrictEqual(d2j(j2d(1400, m, 1)), { jy: 1400, jm: m, jd: 1 });
-      assert.deepStrictEqual(d2j(j2d(1400, m - 12, 1)), {
+      expect(d2j(j2d(1400, m, 1))).toEqual({ jy: 1400, jm: m, jd: 1 });
+      expect(d2j(j2d(1400, m - 12, 1))).toEqual({
         jy: 1399,
         jm: m,
         jd: 1,
       });
-      assert.deepStrictEqual(d2j(j2d(1400, m - 24, 1)), {
+      expect(d2j(j2d(1400, m - 24, 1))).toEqual({
         jy: 1398,
         jm: m,
         jd: 1,
@@ -102,30 +101,30 @@ describe("j2d/d2j", () => {
     for (let i = START; i < END; i++) {
       const { gy, gm, gd } = d2g(i);
       const j = g2d(gy, gm, gd);
-      assert.strictEqual(j, i, `i=${i}, j=${j}, gy=${gy}, gm=${gm}, gd=${gd}`);
+      expect(j).toBe(i);
     }
   });
   it("should handle negative/overflow month", () => {
     for (let m = 1; m <= 12; m++) {
       // for (let d = 1; d <= 28; d++) {
       const d = 1;
-      assert.deepStrictEqual(d2g(g2d(2000, m + 24, d)), {
+      expect(d2g(g2d(2000, m + 24, d))).toEqual({
         gy: 2002,
         gm: m,
         gd: d,
       });
-      assert.deepStrictEqual(d2g(g2d(2000, m + 12, d)), {
+      expect(d2g(g2d(2000, m + 12, d))).toEqual({
         gy: 2001,
         gm: m,
         gd: d,
       });
-      assert.deepStrictEqual(d2g(g2d(2000, m, d)), { gy: 2000, gm: m, gd: d });
-      assert.deepStrictEqual(d2g(g2d(2000, m - 12, d)), {
+      expect(d2g(g2d(2000, m, d))).toEqual({ gy: 2000, gm: m, gd: d });
+      expect(d2g(g2d(2000, m - 12, d))).toEqual({
         gy: 1999,
         gm: m,
         gd: d,
       });
-      assert.deepStrictEqual(d2g(g2d(2000, m - 24, d)), {
+      expect(d2g(g2d(2000, m - 24, d))).toEqual({
         gy: 1998,
         gm: m,
         gd: d,
@@ -138,51 +137,51 @@ describe("j2d/d2j", () => {
 describe("toJalali", () => {
   it("should converts a Gregorian date to Jalaali", () => {
     const result = toJalali(2021, 1, 1);
-    assert.deepStrictEqual(result, { jy: 1399, jm: 10, jd: 12 });
+    expect(result).toEqual({ jy: 1399, jm: 10, jd: 12 });
   });
   it("should converts a leap year correctly", () => {
     const result = toJalali(2021, 3, 20);
-    assert.deepStrictEqual(result, { jy: 1399, jm: 12, jd: 30 });
+    expect(result).toEqual({ jy: 1399, jm: 12, jd: 30 });
   });
   it("should converts a non leap year correctly", () => {
     const result = toJalali(2022, 3, 21);
-    assert.deepStrictEqual(result, { jy: 1401, jm: 1, jd: 1 });
+    expect(result).toEqual({ jy: 1401, jm: 1, jd: 1 });
   });
 });
 
 describe("toGregorian", () => {
   it("should converts a Jalaali date to Gregorian", () => {
     const result = toGregorian(1399, 10, 12);
-    assert.deepStrictEqual(result, { gy: 2021, gm: 1, gd: 1 });
+    expect(result).toEqual({ gy: 2021, gm: 1, gd: 1 });
   });
   it("should converts a leap year correctly", () => {
     const result = toGregorian(1399, 12, 30);
-    assert.deepStrictEqual(result, { gy: 2021, gm: 3, gd: 20 });
+    expect(result).toEqual({ gy: 2021, gm: 3, gd: 20 });
   });
   it("should converts a non leap year correctly", () => {
     const result = toGregorian(1400, 12, 30); // eq 1401, 1, 1
-    assert.deepStrictEqual(result, { gy: 2022, gm: 3, gd: 21 });
+    expect(result).toEqual({ gy: 2022, gm: 3, gd: 21 });
   });
 });
 
 describe("isLeapYear", () => {
   it("should tell correctly", () => {
-    assert.strictEqual(isLeapJalaliYear(1398), false);
-    assert.strictEqual(isLeapJalaliYear(1399), true);
-    assert.strictEqual(isLeapJalaliYear(1400), false);
-    assert.strictEqual(isLeapJalaliYear(1401), false);
-    assert.strictEqual(isLeapJalaliYear(1402), false);
+    expect(isLeapJalaliYear(1398)).toBe(false);
+    expect(isLeapJalaliYear(1399)).toBe(true);
+    expect(isLeapJalaliYear(1400)).toBe(false);
+    expect(isLeapJalaliYear(1401)).toBe(false);
+    expect(isLeapJalaliYear(1402)).toBe(false);
   });
 });
 
 describe("normalizeMonth", () => {
   it("should move between 1..=12", () => {
     for (let m = 1; m <= 12; m++) {
-      assert.deepStrictEqual(normalizeMonth(1400, m + 24), [1402, m]);
-      assert.deepStrictEqual(normalizeMonth(1400, m + 12), [1401, m]);
-      assert.deepStrictEqual(normalizeMonth(1400, m), [1400, m]);
-      assert.deepStrictEqual(normalizeMonth(1400, m - 12), [1399, m]);
-      assert.deepStrictEqual(normalizeMonth(1400, m - 24), [1398, m]);
+      expect(normalizeMonth(1400, m + 24)).toEqual([1402, m]);
+      expect(normalizeMonth(1400, m + 12)).toEqual([1401, m]);
+      expect(normalizeMonth(1400, m)).toEqual([1400, m]);
+      expect(normalizeMonth(1400, m - 12)).toEqual([1399, m]);
+      expect(normalizeMonth(1400, m - 24)).toEqual([1398, m]);
     }
   });
 });
