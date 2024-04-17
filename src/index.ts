8d62443cabbfc625b3b5d7034ad6722b8c29ad33
Sasha Koss <koss@nocorp.me>
2024-04-17 13:00:14 +0800

    (tz): Implement naive timezone scan

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,80 @@
-// TODO:
+export interface Interval {
+  start: Date;
+  end: Date;
+}
+
+export function tzScan(tz: string, interval: Interval) {
+  const changes: any[] = [];
+  const date = new Date(interval.start);
+  date.setUTCSeconds(0, 0);
+  const endDate = new Date(interval.end);
+  endDate.setUTCSeconds(0, 0);
+  const endTime = +endDate;
+
+  const { format } = new Intl.DateTimeFormat("en-US", {
+    timeZone: tz,
+    hour: "numeric",
+    minute: "numeric",
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+    hour12: false,
+  });
+
+  let lastValues = formatToValues(format(date));
+  let lastOffset = calcOffset(lastValues, date);
+  while (+date < endTime) {
+    date.setUTCHours(date.getUTCHours() + 1);
+
+    let lastHours = lastValues[3] + 1;
+    if (lastHours === 24) lastHours = 0;
+
+    const values = formatToValues(format(date));
+
+    if (values[3] !== lastHours) {
+      const offset = calcOffset(values, date);
+      changes.push({
+        date: new Date(date),
+        change: offset - lastOffset,
+        offset,
+      });
+    }
+
+    lastValues = values;
+    lastOffset = calcOffset(lastValues, date);
+  }
+
+  return changes;
+}
+
+export function tzOffset(tz: string, date: Date) {
+  const { format } = new Intl.DateTimeFormat("en-US", {
+    timeZone: tz,
+    hour: "numeric",
+    minute: "numeric",
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+    hour12: false,
+  });
+
+  const _date = new Date(date);
+  _date.setUTCSeconds(0, 0);
+
+  const values = formatToValues(format(_date));
+  return calcOffset(values, _date);
+}
+
+function calcOffset(values: DateValues, date: Date) {
+  return (Date.UTC(...values) - +date) / 60000;
+}
+
+type DateValues = [number, number, number, number, number];
+
+function formatToValues(formatResult: string): DateValues {
+  const [month, day, year, hours, minutes] = formatResult
+    .match(/(\d+)\/(\d+)\/(\d+), (\d+):(\d+)/)
+    ?.slice(1)
+    .map(Number) as DateValues;
+  return [year, month - 1, day, hours === 24 ? 0 : hours, minutes];
+}
---
+++ src/tests.ts
@@ -0,0 +1,108 @@
+import { describe, expect, it } from "vitest";
+import { tzOffset, tzScan } from ".";
+
+describe("tzScan", () => {
+  it("searches for DST changes in the given period", () => {
+    const changes = tzScan("America/New_York", {
+      start: new Date("2020-01-01T00:00:00Z"),
+      end: new Date("2021-12-31T00:00:00Z"),
+    });
+
+    expect(changes).toEqual([
+      {
+        date: new Date("2020-03-08T07:00:00.000Z"),
+        change: 1 * 60,
+        offset: -4 * 60,
+      },
+      {
+        date: new Date("2020-11-01T06:00:00.000Z"),
+        change: -1 * 60,
+        offset: -5 * 60,
+      },
+      {
+        date: new Date("2021-03-14T07:00:00.000Z"),
+        change: 1 * 60,
+        offset: -4 * 60,
+      },
+      {
+        date: new Date("2021-11-07T06:00:00.000Z"),
+        change: -1 * 60,
+        offset: -5 * 60,
+      },
+    ]);
+  });
+
+  it("searches for permanent DST changes in the given period", () => {
+    const changes = tzScan("Turkey", {
+      start: new Date("2015-01-01T00:00:00Z"),
+      end: new Date("2018-12-31T00:00:00Z"),
+    });
+
+    expect(changes).toEqual([
+      {
+        date: new Date("2015-03-29T01:00:00.000Z"),
+        change: 1 * 60,
+        offset: 3 * 60,
+      },
+      {
+        date: new Date("2015-11-08T01:00:00.000Z"),
+        change: -1 * 60,
+        offset: 2 * 60,
+      },
+      {
+        date: new Date("2016-03-27T01:00:00.000Z"),
+        change: 1 * 60,
+        offset: 3 * 60,
+      },
+    ]);
+  });
+
+  it("searches for timezone changes", () => {
+    const changes = tzScan("Asia/Pyongyang", {
+      start: new Date("2010-01-01T00:00:00Z"),
+      end: new Date("2019-12-31T00:00:00Z"),
+    });
+
+    expect(changes).toEqual([
+      {
+        date: new Date("2015-08-14T15:00:00.000Z"),
+        change: -0.5 * 60,
+        offset: 8.5 * 60,
+      },
+      {
+        date: new Date("2018-05-04T15:00:00.000Z"),
+        change: 0.5 * 60,
+        offset: 9 * 60,
+      },
+    ]);
+  });
+});
+
+describe("tzOffset", () => {
+  it("returns the timezone offset for the given date", () => {
+    const date = new Date("2020-01-15T00:00:00Z");
+    expect(tzOffset("America/New_York", date)).toBe(-5 * 60);
+    expect(tzOffset("Asia/Pyongyang", date)).toBe(9 * 60);
+    expect(tzOffset("Asia/Kathmandu", date)).toBe(345);
+  });
+
+  it("works at the end of the day", () => {
+    const date = new Date("2020-01-15T23:59:59Z");
+    expect(tzOffset("America/New_York", date)).toBe(-5 * 60);
+    expect(tzOffset("Asia/Pyongyang", date)).toBe(9 * 60);
+    expect(tzOffset("Asia/Kathmandu", date)).toBe(345);
+  });
+
+  it("works at the end of a month", () => {
+    const date = new Date("2020-01-31T23:59:59Z");
+    expect(tzOffset("America/New_York", date)).toBe(-5 * 60);
+    expect(tzOffset("Asia/Pyongyang", date)).toBe(9 * 60);
+    expect(tzOffset("Asia/Kathmandu", date)).toBe(345);
+  });
+
+  it("works at midnight", () => {
+    expect(tzOffset("America/New_York", new Date("2020-01-15T05:00:00Z"))).toBe(
+      -5 * 60
+    );
+  });
+});
