e96f7586226ba08c67aeba02b9e206a6de009fe7
Sasha Koss <koss@nocorp.me>
2024-04-20 14:03:22 +0800

    (tz): Speed up tzScan 87->5887 (Ryzen 5 5600X)

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,61 @@ export interface Interval {
 
 export function tzScan(tz: string, interval: Interval) {
   const changes: any[] = [];
-  const date = new Date(interval.start);
-  date.setUTCSeconds(0, 0);
+
+  const monthDate = new Date(interval.start);
+  monthDate.setUTCSeconds(0, 0);
+
   const endDate = new Date(interval.end);
   endDate.setUTCSeconds(0, 0);
-  const endTime = +endDate;
 
-  let lastOffset = tzOffset(tz, date);
-  while (+date < endTime) {
-    date.setUTCHours(date.getUTCHours() + 1);
+  const lastMonthTime = +endDate;
+  let lastOffset = tzOffset(tz, monthDate);
+  while (+monthDate < lastMonthTime) {
+    // Month forward
+    monthDate.setUTCMonth(monthDate.getUTCMonth() + 1);
 
-    const offset = tzOffset(tz, date);
-
+    // Find the month where the offset changes
+    const offset = tzOffset(tz, monthDate);
     if (offset != lastOffset) {
-      changes.push({
-        date: new Date(date),
-        change: offset - lastOffset,
-        offset,
-      });
+      // Rewind a month back to find the day where the offset changes
+      const dayDate = new Date(monthDate);
+      dayDate.setUTCMonth(dayDate.getUTCMonth() - 1);
+
+      const lastDayTime = +monthDate;
+      lastOffset = tzOffset(tz, dayDate);
+      while (+dayDate < lastDayTime) {
+        // Day forward
+        dayDate.setUTCDate(dayDate.getUTCDate() + 1);
+
+        // Find the day where the offset changes
+        const offset = tzOffset(tz, dayDate);
+        if (offset != lastOffset) {
+          // Rewind a day back to find the time where the offset changes
+          const hourDate = new Date(dayDate);
+          hourDate.setUTCDate(hourDate.getUTCDate() - 1);
+
+          const endHourTime = +dayDate;
+          lastOffset = tzOffset(tz, hourDate);
+          while (+hourDate < endHourTime) {
+            // Hour forward
+            hourDate.setUTCHours(hourDate.getUTCHours() + 1);
+
+            // Find the hour where the offset changes
+            const hourOffset = tzOffset(tz, hourDate);
+            if (hourOffset !== lastOffset) {
+              changes.push({
+                date: new Date(hourDate),
+                change: hourOffset - lastOffset,
+                offset: hourOffset,
+              });
+            }
+
+            lastOffset = hourOffset;
+          }
+        }
+
+        lastOffset = offset;
+      }
     }
 
     lastOffset = offset;
