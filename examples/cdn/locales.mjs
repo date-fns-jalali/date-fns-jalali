20638630c64d15bfd055989932ce88807b4b59af
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-06-23 09:38:19 +0330

    .

--- examples/cdn/locales.mjs
+++ examples/cdn/locales.mjs
@@ -2,10 +2,10 @@ import { testScript } from "./dom.mjs";
 
 testScript(["cdn.min.js", "locale/cdn.min.js"], (dom) => {
   const esResult = dom.window.eval(
-    `window.dateFns.formatRelative(window.dateFns.subDays(new Date(1987, 1, 11), 3), new Date(1987, 1, 11), { locale: window.dateFns.locale.es })`,
+    `window.dateFnsJalali.formatRelative(window.dateFnsJalali.subDays(new Date(1987, 1, 11), 3), new Date(1987, 1, 11), { locale: window.dateFnsJalali.locale.es })`,
   );
   const ruResult = dom.window.eval(
-    `window.dateFns.formatRelative(window.dateFns.subDays(new Date(1987, 1, 11), 3), new Date(1987, 1, 11), { locale: window.dateFns.locale.ru })`,
+    `window.dateFnsJalali.formatRelative(window.dateFnsJalali.subDays(new Date(1987, 1, 11), 3), new Date(1987, 1, 11), { locale: window.dateFnsJalali.locale.ru })`,
   );
   console.log(
     esResult === "el domingo pasado a las 00:00" &&
