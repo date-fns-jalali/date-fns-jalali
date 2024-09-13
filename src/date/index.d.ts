4e7ce1f7a7468334637f0aac0b7a482ee8a24381
Sasha Koss <koss@nocorp.me>
2024-09-13 10:29:18 +0800

    (tz): Add TSDoc to TZDate

--- src/date/index.d.ts
+++ src/date/index.d.ts
@@ -17,18 +17,65 @@ import { constructFromSymbol } from "../constants/index.ts";
  * For the minimal version, see `TZDateMini`.
  */
 export class TZDate extends Date {
+  /**
+   * Constructs a new `TZDate` instance in the system time zone.
+   */
   constructor();
 
+  /**
+   * Constructs a new `TZDate` instance from the date time string and time zone.
+   *
+   * @param dateStr - Date time string to create a new instance from
+   * @param timeZone - Time zone name (IANA or UTC offset)
+   */
   constructor(dateStr: string, timeZone?: string);
 
+  /**
+   * Constructs a new `TZDate` instance from the date object and time zone.
+   *
+   * @param date - Date object to create a new instance from
+   * @param timeZone - Time zone name (IANA or UTC offset)
+   */
   constructor(date: Date, timeZone?: string);
 
+  /**
+   * Constructs a new `TZDate` instance from the Unix timestamp in milliseconds
+   * and time zone.
+   *
+   * @param timestamp - Unix timestamp in milliseconds to create a new instance from
+   * @param timeZone - Time zone name (IANA or UTC offset)
+   */
   constructor(timestamp: number, timeZone?: string);
 
+  /**
+   * Constructs a new `TZDate` instance from the year, month, and time zone.
+   *
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param timeZone - Time zone name (IANA or UTC offset)
+   */
   constructor(year: number, month: number, timeZone?: string);
 
+  /**
+   * Constructs a new `TZDate` instance from the year, month, date and time zone.
+   *
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param date - Date
+   * @param timeZone - Time zone name (IANA or UTC offset)
+   */
   constructor(year: number, month: number, date: number, timeZone?: string);
 
+  /**
+   * Constructs a new `TZDate` instance from the year, month, date, hours
+   * and time zone.
+   *
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param date - Date
+   * @param hours - Hours
+   * @param timeZone - Time zone name (IANA or UTC offset)
+   */
   constructor(
     year: number,
     month: number,
@@ -37,6 +84,17 @@ export class TZDate extends Date {
     timeZone?: string
   );
 
+  /**
+   * Constructs a new `TZDate` instance from the year, month, date, hours,
+   * minutes and time zone.
+   *
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param date - Date
+   * @param hours - Hours
+   * @param minutes - Minutes
+   * @param timeZone - Time zone name (IANA or UTC offset)
+   */
   constructor(
     year: number,
     month: number,
@@ -46,6 +104,18 @@ export class TZDate extends Date {
     timeZone?: string
   );
 
+  /**
+   * Constructs a new `TZDate` instance from the year, month, date, hours,
+   * minutes, seconds and time zone.
+   *
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param date - Date
+   * @param hours - Hours
+   * @param minutes - Minutes
+   * @param seconds - Seconds
+   * @param timeZone - Time zone name (IANA or UTC offset)
+   */
   constructor(
     year: number,
     month: number,
@@ -56,6 +126,19 @@ export class TZDate extends Date {
     timeZone?: string
   );
 
+  /**
+   * Constructs a new `TZDate` instance from the year, month, date, hours,
+   * minutes, seconds, milliseconds and time zone.
+   *
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param date - Date
+   * @param hours - Hours
+   * @param minutes - Minutes
+   * @param seconds - Seconds
+   * @param milliseconds - Milliseconds
+   * @param timeZone - Time zone name (IANA or UTC offset)
+   */
   constructor(
     year: number,
     month: number,
@@ -67,18 +150,70 @@ export class TZDate extends Date {
     timeZone?: string
   );
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone.
+   *
+   * @param tz - Time zone name (IANA or UTC offset)
+   */
   static tz(tz: string): TZDate;
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone from the Unix
+   * timestamp in milliseconds.
+   *
+   * @param tz - Time zone name (IANA or UTC offset)
+   * @param timestamp - Unix timestamp in milliseconds
+   */
   static tz(tz: string, timestamp: number): TZDate;
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone from the date time
+   * string.
+   *
+   * @param tz - Time zone name (IANA or UTC offset)
+   * @param dateStr - Date time string
+   */
   static tz(tz: string, dateStr: string): TZDate;
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone from the date object.
+   *
+   * @param tz - Time zone name (IANA or UTC offset)
+   * @param date - Date object
+   */
   static tz(tz: string, date: Date): TZDate;
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone from the year
+   * and month.
+   *
+   * @param tz - Time zone name (IANA or UTC offset)
+   * @param year - Year
+   * @param month - Month (0-11)
+   */
   static tz(tz: string, year: number, month: number): TZDate;
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone from the year,
+   * month and date.
+   *
+   * @param tz - Time zone name (IANA or UTC offset)
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param date - Date
+   */
   static tz(tz: string, year: number, month: number, date: number): TZDate;
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone from the year,
+   * month, date and hours.
+   *
+   * @param tz - Time zone name (IANA or UTC offset)
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param date - Date
+   * @param hours - Hours
+   */
   static tz(
     tz: string,
     year: number,
@@ -87,6 +222,17 @@ export class TZDate extends Date {
     hours: number
   ): TZDate;
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone from the year,
+   * month, date, hours and minutes.
+   *
+   * @param tz - Time zone name (IANA or UTC offset)
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param date - Date
+   * @param hours - Hours
+   * @param minutes - Minutes
+   */
   static tz(
     tz: string,
     year: number,
@@ -96,6 +242,18 @@ export class TZDate extends Date {
     minutes: number
   ): TZDate;
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone from the year,
+   * month, date, hours, minutes and seconds.
+   *
+   * @param tz - Time zone name (IANA or UTC offset)
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param date - Date
+   * @param hours - Hours
+   * @param minutes - Minutes
+   * @param seconds - Seconds
+   */
   static tz(
     tz: string,
     year: number,
@@ -106,6 +264,19 @@ export class TZDate extends Date {
     seconds: number
   ): TZDate;
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone from the year,
+   * month, date, hours, minutes, seconds and milliseconds.
+   *
+   * @param tz - Time zone name (IANA or UTC offset)
+   * @param year - Year
+   * @param month - Month (0-11)
+   * @param date - Date
+   * @param hours - Hours
+   * @param minutes - Minutes
+   * @param seconds - Seconds
+   * @param milliseconds - Milliseconds
+   */
   static tz(
     tz: string,
     year: number,
@@ -122,7 +293,16 @@ export class TZDate extends Date {
    */
   readonly timeZone: string | undefined;
 
+  /**
+   * Creates a new `TZDate` instance in the given time zone.
+   */
   withTimeZone(timeZone: string): TZDate;
 
+  /**
+   * Creates a new `TZDate` instance in the current instance time zone and
+   * the specified date time value.
+   *
+   * @param date - Date value to create a new instance from
+   */
   [constructFromSymbol](date: Date | number | string): TZDate;
 }
