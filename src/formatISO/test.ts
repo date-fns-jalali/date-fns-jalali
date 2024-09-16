0e060a648be708a71242dc34b49443f0f6486c94
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-09-16 23:50:14 +0330

    FORMAT: format files

--- src/formatISO/test.ts
+++ src/formatISO/test.ts
@@ -16,7 +16,9 @@ describe("formatISO", () => {
 
     getTimezoneOffsetStub.returns(480);
     const tzNegativeOffsetExtended = generateOffset(date);
-    expect(formatISO(date)).toBe(`2019-03-03T19:00:52${tzNegativeOffsetExtended}`);
+    expect(formatISO(date)).toBe(
+      `2019-03-03T19:00:52${tzNegativeOffsetExtended}`,
+    );
 
     getTimezoneOffsetStub.returns(0);
     const tzZOffsetExtended = generateOffset(date);
@@ -34,22 +36,32 @@ describe("formatISO", () => {
   it("formats ISO-8601 basic format", () => {
     const date = new Date(2019, 9 /* Oct */, 4, 12, 30, 13, 456);
     const tzOffsetBasic = generateOffset(date);
-    expect(formatISO(date, { format: "basic" })).toBe(`20191004T123013${tzOffsetBasic}`);
+    expect(formatISO(date, { format: "basic" })).toBe(
+      `20191004T123013${tzOffsetBasic}`,
+    );
   });
 
   it("formats only date", () => {
     const date = new Date(2019, 11 /* Dec */, 11, 1, 0, 0, 789);
 
-    expect(formatISO(date, { representation: "date", format: "extended" })).toBe("2019-12-11");
-    expect(formatISO(date, { representation: "date", format: "basic" })).toBe("20191211");
+    expect(
+      formatISO(date, { representation: "date", format: "extended" }),
+    ).toBe("2019-12-11");
+    expect(formatISO(date, { representation: "date", format: "basic" })).toBe(
+      "20191211",
+    );
   });
 
   it("formats only time", () => {
     const date = new Date(2019, 2 /* Mar */, 3, 19, 0, 52, 123);
     const tzOffset = generateOffset(date);
 
-    expect(formatISO(date, { representation: "time", format: "extended" })).toBe(`19:00:52${tzOffset}`);
-    expect(formatISO(date, { representation: "time", format: "basic" })).toBe(`190052${tzOffset}`);
+    expect(
+      formatISO(date, { representation: "time", format: "extended" }),
+    ).toBe(`19:00:52${tzOffset}`);
+    expect(formatISO(date, { representation: "time", format: "basic" })).toBe(
+      `190052${tzOffset}`,
+    );
   });
 
   it("throws RangeError if the time value is invalid", () => {
