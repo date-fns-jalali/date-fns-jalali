2eb7416f5194fc9063625d5ae5841aa8010d870f
Sasha Koss <koss@nocorp.me>
2025-08-01 08:05:21 +0000

    (tz): Add engines edge case test

--- test/engines/src/test.ts
+++ test/engines/src/test.ts
@@ -5,6 +5,7 @@ import { TZDate, tzOffset } from "../../../src/index.ts";
   assertEqual(tzOffset("Asia/Singapore", date), 480);
   assertEqual(tzOffset("Asia/Katmandu", date), 345);
 }
+
 {
   const date = new TZDate(2022, 2, 13, "Asia/Singapore");
   assertEqual(date.toISOString(), "2022-03-13T00:00:00.000+08:00");
@@ -17,4 +18,11 @@ import { TZDate, tzOffset } from "../../../src/index.ts";
 function assertEqual<Type>(received: Type, expected: Type) {
   if (received === expected) return;
   throw new Error(`Expected "${expected}", but got "${received}"`);
+}
+
+// Edge case, see: https://github.com/date-fns/tz/issues/32#issuecomment-2832031823
+{
+  const date = new Date(2022, 2, 13);
+  assertEqual(tzOffset("Asia/Kolkata", date), 330);
+  assertEqual(tzOffset("Asia/Calcutta", date), 330);
 }
