c4b5577fa415e7043dbbfaae00f0302f2d6670c4
Sasha Koss <koss@nocorp.me>
2025-08-01 07:46:25 +0000

    (tz): Simplify Format.JS polyfills in the Hermes engine tests

--- test/engines/src/hermes.ts
+++ test/engines/src/hermes.ts
@@ -6,6 +6,6 @@ import "@formatjs/intl-numberformat/polyfill";
 import "@formatjs/intl-numberformat/locale-data/en";
 import "@formatjs/intl-datetimeformat/polyfill";
 import "@formatjs/intl-datetimeformat/locale-data/en";
-import "@formatjs/intl-datetimeformat/add-all-tz";
+import "@formatjs/intl-datetimeformat/add-golden-tz"; // or: "@formatjs/intl-datetimeformat/add-all-tz"
 // Tests
 import "./test.ts";
