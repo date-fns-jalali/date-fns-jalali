7bdcf2c0965c763cb373f1ac44435b51468ff9c4
Sasha Koss <koss@nocorp.me>
2026-05-19 14:51:02 +0800

    Update time zone tests to complete run before exiting

--- pkgs/core/scripts/test/_lib/tz.ts
+++ pkgs/core/scripts/test/_lib/tz.ts
@@ -2,21 +2,44 @@ import { $ } from "bun";
 import { availableParallelism } from "node:os";
 import { promiseQueue } from "./queue.ts";
 
+const failedTimeZones: Record<string, string> = {};
+
 export async function testTimeZone(timeZone: string) {
   try {
     await $`TZ=${timeZone} bun test ./src/**/test.ts`.quiet();
+    console.log(`🟢 ${timeZone}: OK`);
   } catch (err: any) {
-    console.log(err.stderr.toString());
-    console.log(`⛔ Tests for the time zone ${timeZone}`);
-    process.exit(err.exitCode);
+    failedTimeZones[timeZone] = err.stderr.toString();
+    console.log(`🔴 ${timeZone}: FAIL`);
   }
-
-  console.log(`✅ Tests for the time zone ${timeZone}`);
 }
 
-export function testTimeZones(timeZones: string[]) {
-  return promiseQueue(
+export async function testTimeZones(timeZones: string[]) {
+  await promiseQueue(
     timeZones.map((timeZone) => () => testTimeZone(timeZone)),
     availableParallelism(),
   );
+
+  console.log("");
+
+  const failedEntries = Object.entries(failedTimeZones);
+
+  if (failedEntries.length) {
+    failedEntries.forEach(([timeZone, error]) => {
+      console.log(`🔴 ${timeZone}`);
+      console.log("=== stderr ==================================");
+      console.log(error);
+      console.log("=============================================\n");
+    });
+
+    console.log(`🔴 ${failedEntries.length} time zones failed:\n`);
+
+    failedEntries.forEach(([timeZone]) => {
+      console.log(`- ${timeZone} (\`TZ=${timeZone} pnpm vitest run\`)`);
+    });
+
+    process.exit(1);
+  }
+
+  console.log(`🟢 All ${timeZones.length} time zones passed!`);
 }
