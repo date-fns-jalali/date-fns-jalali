e02ba7c027de3f875a69da3e4a9fa788d9e498e6
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2026-05-27 02:51:51 +0330

    REMOVE_LOCALES: remove other locales

--- src/locale/fa-IR/index.ts
+++ src/locale/fa-IR/index.ts
@@ -1,26 +1,27 @@
-import type { Locale } from "../types.ts";
 import { formatDistance } from "./_lib/formatDistance/index.ts";
 import { formatLong } from "./_lib/formatLong/index.ts";
 import { formatRelative } from "./_lib/formatRelative/index.ts";
 import { localize } from "./_lib/localize/index.ts";
 import { match } from "./_lib/match/index.ts";
+import type { Locale } from "../types.ts";
 
 /**
  * @category Locales
- * @summary Persian/Farsi locale (Iran).
- * @language Persian
- * @iso-639-2 ira
- * @author Morteza Ziyae [@mort3za](https://github.com/mort3za)
+ * @summary English locale (United States).
+ * @language English
+ * @iso-639-2 eng
+ * @author Sasha Koss [@kossnocorp](https://github.com/kossnocorp)
+ * @author Lesha Koss [@leshakoss](https://github.com/leshakoss)
  */
 export const faIR: Locale = {
-  code: "fa-IR",
+  code: "en-US",
   formatDistance: formatDistance,
   formatLong: formatLong,
   formatRelative: formatRelative,
   localize: localize,
   match: match,
   options: {
-    weekStartsOn: 6 /* Saturday */,
+    weekStartsOn: 0 /* Sunday */,
     firstWeekContainsDate: 1,
   },
 };
